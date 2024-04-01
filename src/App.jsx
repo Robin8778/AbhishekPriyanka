29042ca03b883042d5a0bb8991edcf9d52683e04
robin8778 <robin.007660@gmail.com>
2024-04-01 20:12:25 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { Gallery } from "./Gallery"
+import "./styles.css"
+
+
+function App() {
+
+
+  return (
+    <>
+      <header style={{ display: 'flex', justifyContent: "center", alignItems: "center", color: "white", fontSize: "20px", paddingBottom: "10px", fontFamily: "fantasy" }}>
+        Abhishek ❤️ Priyanka
+      </header>
+      <Gallery />
+    </>
+  )
+}
+
+export default App
---
+++ src/Gallery.jsx
@@ -0,0 +1,45 @@
+import LightGallery from 'lightgallery/react';
+
+// import styles
+import 'lightgallery/css/lightgallery.css';
+import 'lightgallery/css/lg-zoom.css';
+import 'lightgallery/css/lg-thumbnail.css';
+import 'lightgallery/css/lg-autoplay.css';
+import 'lightgallery/css/lg-fullscreen.css';
+import 'lightgallery/css/lg-share.css';
+import 'lightgallery/css/lg-rotate.css';
+
+
+// import plugins if you need
+import lgThumbnail from 'lightgallery/plugins/thumbnail';
+import lgZoom from 'lightgallery/plugins/zoom';
+import lgAutoplay from 'lightgallery/plugins/autoplay'
+import lgFullscreen from 'lightgallery/plugins/fullscreen';
+import lgShare from 'lightgallery/plugins/share';
+import lgRotate from 'lightgallery/plugins/rotate';
+
+export function Gallery() {
+    const onInit = () => {
+        console.log('lightGallery has been initialized');
+    };
+    return (
+        <div className="App">
+            <LightGallery
+                onInit={onInit}
+                speed={500}
+                plugins={[lgThumbnail, lgZoom, lgAutoplay, lgFullscreen, lgRotate]}
+            >
+
+                {Array.from({ length: 38 }).map((_, index) => {
+                    return (
+                        <a href={`/${index + 1}.jpg`} key={index + 1}>
+                            <img alt={`Wedding`} src={`/${index + 1}.jpg`} />
+                        </a>
+                    );
+                })}
+
+
+            </LightGallery>
+        </div>
+    );
+}
